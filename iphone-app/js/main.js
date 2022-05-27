755ba75beb9e0f44099173350fe7a06c920ab415
houcem <hedhoucem@gmail.com>
2022-05-27 14:15:42 +0100

    product name and description

---
+++ iphone-app/js/main.js
@@ -0,0 +1,10 @@
+const myApp = Vue.createApp({
+  data() {
+    return {
+      product: "iPhone 13",
+      description:
+        "iPhone 13 is the latest iPhone in the iPhone series. It is powered by a 2.84-GHz dual-core processor and has a camera system that can take up to 8 photos in a single burst.",
+    };
+  },
+});
+myApp.mount("#app");
