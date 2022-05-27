775ec8e1215496dd1a1210ec33dc2077cb9100b4
houcem <hedhoucem@gmail.com>
2022-05-27 14:38:50 +0100

    bind src image

--- iphone-app/js/main.js
+++ iphone-app/js/main.js
@@ -4,6 +4,7 @@ const myApp = Vue.createApp({
       product: "iPhone 13",
       description:
         "iPhone 13 is the latest iPhone in the iPhone series. It is powered by a 2.84-GHz dual-core processor and has a camera system that can take up to 8 photos in a single burst.",
+      image: "./images/iphone-13-blue.png",
     };
   },
 });
