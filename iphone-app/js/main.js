1a67dbd86de2c7ad8eb117552c7f73c847fc9b87
houcem <hedhoucem@gmail.com>
2022-05-30 16:28:28 +0100

    review zone component

--- iphone-app/js/main.js
+++ iphone-app/js/main.js
@@ -2,6 +2,7 @@ import app from "./app.js";
 import navbarComponent from "./modules/Navbar.js";
 import cartComponent from "./modules/Cart.js";
 import productComponent from "./modules/Product.js";
+import reviewZoneComponent from "./modules/review/ReviewZone.js";
 
 // const myApp = Vue.createApp(app);
 // myApp.component("navbar-component", navbarComponent);
@@ -13,4 +14,5 @@ Vue.createApp(app)
   .component("navbar-component", navbarComponent)
   .component("cart-component", cartComponent)
   .component("product-component", productComponent)
+  .component("review-zone-component", reviewZoneComponent)
   .mount("#app");
---
+++ iphone-app/js/modules/review/ReviewZone.js
@@ -0,0 +1,8 @@
+export default {
+  template: `	
+        <div class="review-zone">
+            <h1>ReviewForm Component</h1>
+            <h1>ReviewList Component</h1>
+        </div>
+    `,
+};
