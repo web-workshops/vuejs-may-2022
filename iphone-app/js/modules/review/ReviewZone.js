b3839cd2f078a5f7b31770bccd77341e096719df
houcem <hedhoucem@gmail.com>
2022-05-30 16:33:28 +0100

    reviewForm & reviewList components

--- iphone-app/js/modules/review/ReviewZone.js
+++ iphone-app/js/modules/review/ReviewZone.js
@@ -1,8 +1,15 @@
+import reviewForm from "./ReviewForm.js";
+import reviewList from "./ReviewList.js";
+
 export default {
   template: `	
         <div class="review-zone">
-            <h1>ReviewForm Component</h1>
-            <h1>ReviewList Component</h1>
+            <review-form></review-form>
+            <review-list></review-list>
         </div>
     `,
+  components: {
+    reviewForm,
+    reviewList,
+  },
 };
