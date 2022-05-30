81ba7a5c425ecfa19e58613d8d0f9f801210a95b
houcem <hedhoucem@gmail.com>
2022-05-30 14:55:26 +0100

    export navbar to module

--- iphone-app/js/main.js
+++ iphone-app/js/main.js
@@ -1,3 +1,5 @@
+import navbarComponent from "./modules/Navbar.js";
+
 const myApp = Vue.createApp({
   data() {
     return {
@@ -5,9 +7,8 @@ const myApp = Vue.createApp({
     };
   },
 });
-myApp.component("navbar-component", {
-  template: `<div class="nav-bar"></div>`,
-});
+myApp.component("navbar-component", navbarComponent);
+
 myApp.component("cart-component", {
   template: `<div class="cart">Cart({{ cart }})</div>`,
   props: ["cart"],
