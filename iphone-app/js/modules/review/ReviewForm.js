d033e175a9cb2e15d938e2a5034540f3b63d394f
houcem <hedhoucem@gmail.com>
2022-05-30 16:36:00 +0100

    create review form

--- iphone-app/js/modules/review/ReviewForm.js
+++ iphone-app/js/modules/review/ReviewForm.js
@@ -1,3 +1,17 @@
 export default {
-  template: `<h1>ReviewForm Component</h1>`,
+  template: `
+    <form class="review-form">
+        <h3>Drop a review</h3>
+        <label for="pseudo">Pseudo:</label>
+        <input v-model="pseudo" type="text">
+
+        <label for="review">Your review:</label>
+        <textarea v-model="review" id="review"></textarea>
+
+        <label for="rating">Rating</label>
+        <input v-model.number="rating" type="range" id="rating" min="1" max="5">
+
+        <input type="submit" value="Submit" class="button">
+    </form>
+  `,
 };
