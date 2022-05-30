2a88cae6cc918518048881cb5d3bf81af206973f
houcem <hedhoucem@gmail.com>
2022-05-30 15:25:09 +0100

    use chaining functions

--- iphone-app/js/main.js
+++ iphone-app/js/main.js
@@ -3,10 +3,14 @@ import navbarComponent from "./modules/Navbar.js";
 import cartComponent from "./modules/Cart.js";
 import productComponent from "./modules/Product.js";
 
-const myApp = Vue.createApp(app);
+// const myApp = Vue.createApp(app);
+// myApp.component("navbar-component", navbarComponent);
+// myApp.component("cart-component", cartComponent);
+// myApp.component("product-component", productComponent);
+// myApp.mount("#app");
 
-myApp.component("navbar-component", navbarComponent);
-myApp.component("cart-component", cartComponent);
-myApp.component("product-component", productComponent);
-
-myApp.mount("#app");
+Vue.createApp(app)
+  .component("navbar-component", navbarComponent)
+  .component("cart-component", cartComponent)
+  .component("product-component", productComponent)
+  .mount("#app");
