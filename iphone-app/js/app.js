13663417b6e981142045c759c5ca0172640b7626
houcem <hedhoucem@gmail.com>
2022-05-30 15:23:02 +0100

    move root component to seperate module

---
+++ iphone-app/js/app.js
@@ -0,0 +1,7 @@
+export default {
+  data() {
+    return {
+      cart: 0,
+    };
+  },
+};
--- iphone-app/js/main.js
+++ iphone-app/js/main.js
@@ -1,14 +1,10 @@
+import app from "./app.js";
 import navbarComponent from "./modules/Navbar.js";
 import cartComponent from "./modules/Cart.js";
 import productComponent from "./modules/Product.js";
 
-const myApp = Vue.createApp({
-  data() {
-    return {
-      cart: 0,
-    };
-  },
-});
+const myApp = Vue.createApp(app);
+
 myApp.component("navbar-component", navbarComponent);
 myApp.component("cart-component", cartComponent);
 myApp.component("product-component", productComponent);
