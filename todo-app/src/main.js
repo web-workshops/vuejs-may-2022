86b1efa5a88e1cb9bcf6870aaf5eddc24ccf910e
houcem <hedhoucem@gmail.com>
2022-05-31 14:11:16 +0100

    import bootstrap in main

--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
