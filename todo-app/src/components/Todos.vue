1200da3e0a4ebd653168238d44022baf3a0bafe0
houcem <hedhoucem@gmail.com>
2022-05-31 16:16:09 +0100

    get todos from server

---
+++ todo-app/src/components/Todos.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="todo in todos" :key="todo.id">
+      {{ todo.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  created() {
+    this.getTodos();
+  },
+  methods: {
+    getTodos() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+        .then((response) => {
+          this.todos = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
